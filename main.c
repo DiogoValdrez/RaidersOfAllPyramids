61e55603dba41e51d2eabb0307856419133f718a
plr220 <pedroraposo220@gmail.com>
2021-10-06 14:29:54 +0100

    Added file input control and tests

--- main.c
+++ main.c
@@ -10,16 +10,29 @@ int main(int argc, char *argv[])
 {
     int opt;
     int FirstSubmission = 0; //Variable that holds info about -s option
+    char file_read[24];
+    int i = -1;
 
-    while((opt = getopt(argc, argv, LISTA_OPCOES)) != -1)
-	{
-	  switch (opt)
-      {
-          case 's':
-            FirstSubmission = 1;
-          
-      }
-    }
-    
-    return 0;
+  opt = getopt(argc, argv,"s");
+  if (opt != -1 && opt == 's')
+  {
+      FirstSubmission = 1;
+      printf("fd");
+  }
+
+  if (argv[optind] != NULL)
+  {
+    strcpy(file_read, argv[optind]);
+    //printf("%s", file_read);
+  }
+  
+  char *last4i = &file_read[strlen(file_read)-3];
+  if(strncmp(last4i, ".in", -3) != 0)
+  {
+    return EXIT_FAILURE;
+  }
+
+  
+  
+  return 0;
 }
