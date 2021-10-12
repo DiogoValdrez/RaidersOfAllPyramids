1252a2bcc89f3003f5b46f9d32bd459bb100a134
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-10-12 15:44:57 +0100

    Added loop for reading

--- file_handling.c
+++ file_handling.c
@@ -9,43 +9,47 @@ int** createBoard(char filename[24], int *varia, char *varia2){
   int numb;
   int a;
   int b;
-
   fp = fopen(filename, "r");//fazer verificações
   fscanf(fp, "%d" , size);//em vez de ler palavra a palavra, ler linha a linha, ou afinal não
-  fscanf(fp, "%d" , &(size[1]));
+  do{
+    fscanf(fp, "%d" , &(size[1]));
 
-  bd = (int **)malloc(sizeof(int*)*size[0]);//fazer verificações
-  for(i = 0; i<size[0]; i++){
-    bd[i] = (int*)malloc(sizeof(int)*size[1]);
-  } 
-  for(i = 0; i<size[0]; i++){
-    for (j = 0; j<size[1]; j++){
-      bd[i][j] = 0;
+    bd = (int **)malloc(sizeof(int*)*size[0]);//fazer verificações
+    for(i = 0; i<size[0]; i++){
+      bd[i] = (int*)malloc(sizeof(int)*size[1]);
+    } 
+    for(i = 0; i<size[0]; i++){
+      for (j = 0; j<size[1]; j++){
+        bd[i][j] = 0;
+      }
     }
-  }
 
 
-  //ter atenção para caso não haver uma 3 coluna
-  //por if variante
-  //o prof não tinha um mostrador de labirinto?
-  //ter atenção caso o numb esteja errado
-  //verificar os valores do fscanf
-  for(i = 0; i<2; i++){
-    fscanf(fp, "%d" , &(varia[i]));
-  }
-  fscanf(fp, "%s", varia2);
-  fscanf(fp, "%d", &numb);
-  for(i = 0; i<numb; i++){
-    fscanf(fp, "%d", &a);
-    fscanf(fp, "%d", &b);
-    fscanf(fp, "%d", &(bd[a-1][b-1]));
-  }
-  for(i = 0; i<size[0]; i++){
-    for (j = 0; j<size[1]; j++){
-      printf("%d ", bd[i][j]);
+    //ter atenção para caso não haver uma 3 coluna
+    //por if variante
+    //o prof não tinha um mostrador de labirinto?
+    //ter atenção caso o numb esteja errado
+    //verificar os valores do fscanf
+    for(i = 0; i<2; i++){
+      fscanf(fp, "%d" , &(varia[i]));
     }
-    printf("\n");
-  }
+    fscanf(fp, "%s", varia2);
+    fscanf(fp, "%d", &numb);
+    for(i = 0; i<numb; i++){
+      fscanf(fp, "%d", &a);
+      fscanf(fp, "%d", &b);
+      fscanf(fp, "%d", &(bd[a-1][b-1]));
+    }
+    for(i = 0; i<size[0]; i++){
+      for (j = 0; j<size[1]; j++){
+        printf("%d ", bd[i][j]);
+      }
+      printf("\n");
+    }
+
+  }while(fscanf(fp, "%d" , size)==1);//ver se se escreve assim o do while
+  //ter atenção, chamar aqui a função das variantes e dar free, antes de fazer a proxima tabela
+
   fclose(fp);
   return bd;
 }
