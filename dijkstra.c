4fd15c6665eaea85657a22751f16bf50b987f79e
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-11-10 12:51:47 +0000

    Added heap, lets see if it works some time

--- dijkstra.c
+++ dijkstra.c
@@ -1,52 +1,92 @@
 #include "dijkstra.h"
 
 void Dijkstra(int **cost, int x, int start, int* distance, int* pred, int* visited) {//tirar os tart e por apenas 0
-    int count, mindistance, i, j;
-    int nextnode = 0;
+    int count, mindistance, i, j;//count n é necessario provavekmente
+    heap** array = malloc(sizeof(heap*)*x);
+    int size = 0;
+    int u;
+    heap* h;
 
     // Creating cost matrix
-    for (i = 0; i < x; i++)
+    for (i = 0; i < x; i++)//apagar tralha a mais
         for (j = 0; j < x; j++)
         if (cost[i][j] == -1)
             cost[i][j] = INT_MAX;//por -1 logo na matriz de adjacencias
 
     for (i = 0; i < x; i++) {
-        distance[i] = cost[start][i];
+        distance[i] = INT_MAX;//aqui pode dar problemas pq esta diferente do codigo antigo
         pred[i] = start;
         visited[i] = 0;
-    }
+        h = (heap*)malloc(sizeof(heap));
+        h->V = i;
+        h->value = distance[i];
+        insert(array,h,&size);
 
+        
+    }   
     distance[start] = 0;
     visited[start] = 1;
     count = 1;
 
-    /* print da matriz de custos
-    for (i = 0; i < x; i++){
+    //print da matriz de custos
+    /* for (i = 0; i < x; i++){
         for (j = 0; j < x; j++){
             printf("%d ", cost[i][j]);
         }
         printf("\n");
-    }*/
+    } */
 
-    while (count < x - 1) {
-        mindistance = INT_MAX;
 
-        for (i = 0; i < x; i++)
-        if (( distance[i] < mindistance && !visited[i])&&distance[i] > 0) {//a partir de aqui ter em atenção que temos de verificar todos os -1 em vez de ser só menor
-            mindistance = distance[i];
-            nextnode = i;
-        }
+    printArray(array,&size);
+    printf("\n");
+    while(size > 0){
+      //mindistance = INT_MAX;
+      
+      u = deleteRoot(array, 0, &size);
+      //mindistance = distance[u];
 
-        visited[nextnode] = 1;
-        for (i = 0; i < x; i++)
-        if (!visited[i]){
-            if ((mindistance + cost[nextnode][i] < distance[i])&& mindistance + cost[nextnode][i] > 0){//aqui
-            distance[i] = mindistance + cost[nextnode][i];
-            pred[i] = nextnode;
-            }
-        }
-        count++;
+      visited[u]=1;
+      printf("%d|", distance[u]);
+
+      for(i = 0; i < x; i++){//vizinhos de u????
+        if(!visited[i]){
+          if((distance[u] + cost[u][i] < distance[i])&& distance[u] + cost[u][i] > 0){
+            distance[i] = distance[u] + cost[u][i];
+            pred[i] = u;
+          }
+        }      
+      }     
+      count++;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* while(size > 0){
+      mindistance = INT_MAX;
+      u = deleteRoot(array, 0, &size);
+      if(distance[u]!=INT_MAX){
+        for(i = 0; i < x; i++){//vizinhos de u????
+          if(cost[u][i]!=INT_MAX && cost[u][i]!=0){//necessario
+            mindistance = distance[u] + cost[u][i];
+            if(mindistance < distance[i]){//tirar min > o
+              distance[i] = mindistance;
+              pred[i] = u;
+            }//talvez aqui um else
+          }       
+        }
+      }     
+      count++;
+    } */
 
     // Printing the distance
     for (i = 0; i < x; i++){
@@ -57,6 +97,7 @@ void Dijkstra(int **cost, int x, int start, int* distance, int* pred, int* visit
         //printf("\nDistance from source to %d: %d\n", i, distance[i]);
         }
     }
+    free(array);
 }
 
 
@@ -68,66 +109,72 @@ void Dijkstra(int **cost, int x, int start, int* distance, int* pred, int* visit
 
 
 
-void swap(int *a, int *b) {
-  int temp = *b;
+void swap(heap **a, heap **b) {
+  heap* temp = *b;
   *b = *a;
   *a = temp;
 }
 
 // Function to heapify the tree
-void heapify(int array[], int size, int i) {
-  if (size == 1) {
-    printf("Single element in the heap");
+void heapify(heap** array, int* size, int i) {
+  if (*size == 1) {
+    //printf("Single element in the heap");
   } else {
     // Find the largest among root, left child and right child
     int largest = i;
     int l = 2 * i + 1;
     int r = 2 * i + 2;
-    if (l < size && array[l] > array[largest])
+    if (l < *size && array[l]->value < array[largest]->value)
       largest = l;
-    if (r < size && array[r] > array[largest])
+    if (r < *size && array[r]->value < array[largest]->value)
       largest = r;
 
     // Swap and continue heapifying if root is not largest
     if (largest != i) {
       swap(&array[i], &array[largest]);
-      heapify(array, size, largest);
+      heapify(array, size, largest);//we dont like recursivas 
     }
   }
 }
 
 // Function to insert an element into the tree
-void insert(int array[], int newNum, int size) {
-  if (size == 0) {
-    array[0] = newNum;
-    size += 1;
+void insert(heap** array, heap* h, int* size) {
+  int i;
+  if (*size == 0) {
+    array[0] = h;
+    *size += 1;
   } else {
-    array[size] = newNum;
-    size += 1;
-    for (int i = size / 2 - 1; i >= 0; i--) {
+    array[*size] = h;
+    *size += 1;
+    for (i = *size / 2 - 1; i >= 0; i--) {
       heapify(array, size, i);
     }
   }
 }
 
 // Function to delete an element from the tree
-void deleteRoot(int array[], int num, int size) {
-  int i;
-  for (i = 0; i < size; i++) {
-    if (num == array[i])
+int deleteRoot(heap** array, int num, int* size) {//modifiquei este tambem
+  int i, r;
+  r = array[num]->V;
+  for (i = 0; i < *size; i++) {
+    if (num == array[i]->V)
       break;
   }
-
-  swap(&array[i], &array[size - 1]);
-  size -= 1;
-  for (int i = size / 2 - 1; i >= 0; i--) {
+  swap(&array[num], &array[*size - 1]);
+  *size -= 1;
+  for (i = *size / 2 - 1; i >= 0; i--) {
     heapify(array, size, i);
   }
+  return r;
 }
 
 // Print the array
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; ++i)
-    printf("%d ", array[i]);
+void printArray(heap** array, int* size) {
+  int i;
+  for (i = 0; i < *size; ++i)
+    printf("%d ", array[i]->value);
   printf("\n");
-}
+  for (i = 0; i < *size; ++i)
+    printf("%d ", array[i]->V);
+  printf("\n");
+} 
--- dijkstra.h
+++ dijkstra.h
@@ -6,14 +6,21 @@
 #include <limits.h>
 #include <stdbool.h>
 
+typedef struct _heap{
+  int V;
+  int value;
+}heap;
+
+
 void Dijkstra(int **Graph, int x, int start, int* distance, int* pred, int* visited);
 
 
-void swap(int *a, int *b);
-void heapify(int array[], int size, int i);
-void insert(int array[], int newNum, int size);
-void deleteRoot(int array[], int num, int size);
-void printArray(int array[], int size);
+void swap(heap **a, heap **b);
+void heapify(heap** array, int* size, int i);
+void insert(heap** array, heap* h, int* size);
+int deleteRoot(heap** array, int num, int* size);
+void printArray(heap** array, int* size);
+
 
 
 #endif
