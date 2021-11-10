47bdaee00bcb3b465d0d93a98125ec13840f2484
Diogo Valdrez <diogovaldrez@tecnico.ulisboa.pt>
2021-11-10 01:53:04 +0000

    added max heap just for helping

--- dijkstra.c
+++ dijkstra.c
@@ -58,3 +58,76 @@ void Dijkstra(int **cost, int x, int start, int* distance, int* pred, int* visit
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+void swap(int *a, int *b) {
+  int temp = *b;
+  *b = *a;
+  *a = temp;
+}
+
+// Function to heapify the tree
+void heapify(int array[], int size, int i) {
+  if (size == 1) {
+    printf("Single element in the heap");
+  } else {
+    // Find the largest among root, left child and right child
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < size && array[l] > array[largest])
+      largest = l;
+    if (r < size && array[r] > array[largest])
+      largest = r;
+
+    // Swap and continue heapifying if root is not largest
+    if (largest != i) {
+      swap(&array[i], &array[largest]);
+      heapify(array, size, largest);
+    }
+  }
+}
+
+// Function to insert an element into the tree
+void insert(int array[], int newNum, int size) {
+  if (size == 0) {
+    array[0] = newNum;
+    size += 1;
+  } else {
+    array[size] = newNum;
+    size += 1;
+    for (int i = size / 2 - 1; i >= 0; i--) {
+      heapify(array, size, i);
+    }
+  }
+}
+
+// Function to delete an element from the tree
+void deleteRoot(int array[], int num, int size) {
+  int i;
+  for (i = 0; i < size; i++) {
+    if (num == array[i])
+      break;
+  }
+
+  swap(&array[i], &array[size - 1]);
+  size -= 1;
+  for (int i = size / 2 - 1; i >= 0; i--) {
+    heapify(array, size, i);
+  }
+}
+
+// Print the array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i)
+    printf("%d ", array[i]);
+  printf("\n");
+}
--- dijkstra.h
+++ dijkstra.h
@@ -8,4 +8,12 @@
 
 void Dijkstra(int **Graph, int x, int start, int* distance, int* pred, int* visited);
 
+
+void swap(int *a, int *b);
+void heapify(int array[], int size, int i);
+void insert(int array[], int newNum, int size);
+void deleteRoot(int array[], int num, int size);
+void printArray(int array[], int size);
+
+
 #endif
